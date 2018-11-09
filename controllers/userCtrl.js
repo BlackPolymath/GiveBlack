d41d451e68bf4fada28d7f4febcb154d571ca540
“Christina <cfthompson321@gmail.com>
2018-11-09 16:46:01 -0500

    added redirect after login route

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -14,10 +14,21 @@ router.get("/", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login.hbs");
 });
+// Post login
+router.post("/login", (req, res) => {
+  user
+    .create({
+      emailuserName: req.body.emailuserName,
+      password: req.body.password
+    })
+    .then(user => {
+      res.redirect("/");
+    });
+});
 
 // GET request/Sign Up => route is to signup.hbs
-router.get("/signup", (req, res) => {
-  res.render("signup.hbs");
-});
+// router.get("/signup", (req, res) => {
+//   res.render("signup.hbs");
+// });
 
 module.exports = router;
